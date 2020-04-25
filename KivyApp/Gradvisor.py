3fbb2c7f9110c49530d6b5b28480c9b65909f3d6
Soujanya <creatingaworldofimagination@gmail.com>
2020-04-25 14:30:45 +0530

    Basic Kivy App

---
+++ KivyApp/Gradvisor.py
@@ -0,0 +1,15 @@
+import kivy
+kivy.require('1.11.1')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class GradvisorApp(App):
+
+	def build(self):
+		return Label(text='Hello World')
+
+
+if __name__ == '__main__':
+	GradvisorApp().run()
